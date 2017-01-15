1a8fa7d0c4d3f1a6d789267530d5b88a8195a672
prakash <prakash.pandey911@gmail.com>
2017-01-15 12:22:46 +0530

    done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "ruby is my love"
+    if page_title.empty?
+      base_title
+    end
+  else
+    page_title + "|" + base_title
+  end
 end
