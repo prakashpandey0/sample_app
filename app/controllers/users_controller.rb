6c4877649df7a313933da143d42cd4ab67cf2a59
prakash <prakash.pandey911@gmail.com>
2017-01-19 12:49:42 +0530

    adding destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index,:edit, :update]        #requiring loged-in user can only edit and update
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
   @user = User.all
@@ -44,6 +45,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  def admin_user
+
+  end
+
     private
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -66,6 +76,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless @user == current_user
   end
+
+
 
 
 end
